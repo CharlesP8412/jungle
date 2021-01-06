22ad7d9d098d41316bee5361d9527c75eea1d134
Charles P <charles.prystupa@gmail.com>
2021-01-06 19:52:06 +0000

    Populate Admin Dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,16 @@
 class Admin::DashboardController < ApplicationController
 
   http_basic_authenticate_with name: ENV['BASIC_AUTH_USER'], password: ENV['BASIC_AUTH_PASS']
-  
+
   def show
+  # @categorie_count = Categorie.count
+  @dashboard_data = {
+    catagories: Category.count,
+    products: Product.count,  
+    orders: Order.count,
+    avg_sale: Order.average('total_cents'),
+    total_sales: Order.sum('total_cents')
+  }
+  # @total_sales = Order.sum(total_cents)
   end
 end
