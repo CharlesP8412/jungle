d1784e88553122005c6668a40ec87993eef390b0
Charles P <charles.prystupa@gmail.com>
2021-01-07 16:46:46 +0000

    Add user authentication
    Broke Admin Dashboard on DB reset (0 sales gives error on AVG Sales)

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,14 @@ Rails.application.routes.draw do
 
   get "/about", to: "about#show"
 
+#===========Login and Session Routes===========================
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+#=============================================================
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
