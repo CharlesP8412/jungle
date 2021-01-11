1425dbad157d70b160eda1aacca8e3626afd4208
Charles P <charles.prystupa@gmail.com>
2021-01-11 22:26:17 +0000

    Basic User specs

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'Validations' do
+    before do
+      @user = User.new(first_name: 'John', last_name: 'Test', email: 'test@test.com', password: '1234', password_confirmation: '1234')
+    end
+    it 'Create user without error' do
+      @user.save
+      expect(@user.errors.full_messages).not_to be_present
+    end
+    it 'Mis-matched passwords produce an error' do
+      @user.password = '1234'
+      @user.password_confirmation = 'ABCD'
+      @user.save
+      expect(@user.errors.full_messages).to be_present
+    end
+    it 'Passwords cannot be blank' do
+      @user.password = nil
+      @user.password_confirmation = nil
+      @user.save
+      expect(@user.errors.full_messages).to be_present
+    end
+    it 'Email must be unique' do
+      @user.save
+      @user = User.new(first_name: 'John', last_name: 'Test', email: 'TEST@TEST.com', password: '1234', password_confirmation: '1234')
+      @user.save
+      # works with exact match but not uppercase
+      expect(@user.errors.full_messages).to be_present
+    end
+    it 'Require First Name' do
+      @user.first_name = nil
+      @user.save
+      expect(@user.errors.full_messages).to be_present
+    end
+    it 'Require Last Name' do
+      @user.last_name = nil
+      @user.save
+      expect(@user.errors.full_messages).to be_present
+    end
+    fit 'Password length must be >= 3 ' do
+      @user.password = '12'
+      @user.password_confirmation = '12'
+      @user.save
+      expect(@user.errors.full_messages).to be_present
+    end
+  end
+end
