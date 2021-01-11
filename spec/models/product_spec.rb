143b158a5f3809a412a37d525183850d816952f0
Charles P <charles.prystupa@gmail.com>
2021-01-11 21:29:52 +0000

    Refactor product validation test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,52 +3,60 @@ require 'rails_helper'
 # validate name, price, qty, category, all presence: true
 
 RSpec.describe Product, type: :model do
-  @params = {
-    name: 'Hotdog Slicer',
-    description: 'Faker::Hipster.paragraph(4)',
-    quantity: 5,
-    price: 26.00
-  }
-  it 'should have a name' do
-    params = {
-      name: 'Hotdog Slicer',
-      description: 'Faker::Hipster.paragraph(4)',
-      quantity: 5,
-      price: 26.00
-    }
-    @product = Product.new(params)
-    expect(@product.name).to be
-  end
-  it 'should have a price' do
-    params = {
-      name: 'Hotdog Slicer',
-      description: 'Faker::Hipster.paragraph(4)',
-      quantity: 5,
-      price: 26.00
-    }
-    @product = Product.new(params)
-
-    expect(@product.price).to be
-  end
-  it 'should have a qty' do
-    params = {
-      name: 'Hotdog Slicer',
-      description: 'Faker::Hipster.paragraph(4)',
-      quantity: 5,
-      price: 26.00
-    }
-    @product = Product.new(params)
-    expect(@product.quantity).to be
-  end
-  it 'should have a category' do
-    params = {
-      name: 'Hotdog Slicer',
-      description: Faker::Hipster.paragraph(4),
-      quantity: 5,
-      category_id: 1,
-      price: 26.00
-    }
-    @product = Product.new(params)
-    expect(@product.category_id).to be
+  describe 'Validations' do
+    it 'should have a name' do
+      params = {
+        name: 'Hotdog Slicer',
+        quantity: 5,
+        category_id: 1,
+        price: 26.00
+      }
+      @product = Product.new(params)
+      @product.save
+      expect(@product.name).to be_present
+    end
+    it 'should have a price' do
+      params = {
+        name: 'Hotdog Slicer',
+        quantity: 5,
+        category_id: 1,
+        price: 26.00
+      }
+      @product = Product.new(params)
+      @product.save
+      expect(@product.price).to be_present
+    end
+    it 'should have a qty' do
+      params = {
+        name: 'Hotdog Slicer',
+        quantity: 5,
+        category_id: 1,
+        price: 26.00
+      }
+      @product = Product.new(params)
+      @product.save
+      expect(@product.quantity).to be_present
+    end
+    it 'should have a category' do
+      params = {
+        name: 'Hotdog Slicer',
+        quantity: 5,
+        category_id: 1,
+        price: 26.00
+      }
+      @product = Product.new(params)
+      @product.save
+      expect(@product.category_id).to be_present
+    end
+    it 'should produce error if empty' do
+      params = {
+        quantity: 5,
+        category_id: 1,
+        price: 26.00
+      }
+      @product = Product.new(params)
+      @product.save
+      expect(@product.errors.full_messages).to be_present
+    end
   end
 end
