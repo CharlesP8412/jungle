3b9eb93b1a5ff7b1c2e666f2aa54cabcdfcf720b
Charles P <charles.prystupa@gmail.com>
2021-01-13 13:55:50 +0000

    Feature Homepage Spec w. Capybara

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,18 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by_email(params[:email])
+    user.email = format_email(user.email)
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to '/'
     else
       redirect_to '/login'
     end
+  end
+
+  def format_email(input_email)
+    input_email = input_email.strip # Remove White space and RegEx
+    input_email.downcase! # Make lowercase
   end
 
   def destroy
